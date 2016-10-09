f4f62ddb9f3c33200baf2da9a7922239e4a57589
Jay McCarthy <jay.mccarthy@gmail.com>
2016-10-08 20:10:03 -0400

    up

--- dept.rkt
+++ dept.rkt
@@ -14,20 +14,45 @@
 (provide (rename-out [dept-module-begin #%module-begin])
          #%datum
          #%app
-         #%top)
+         #%top
+         quote)
 
 ;; Data Model
+(define-syntax-rule (define&provide f x)
+  (begin (define f x)
+         (provide f)))
+(define-syntax-rule (repeat f x ...)
+  (begin (f x) ...))
+
 (struct data (name content))
 (define-syntax-rule (define-data kind)
-  (begin (define (kind . content) (data kind content))
-         (provide kind)))
-(define-syntax-rule (define-data* k ...)
-  (begin (define-data k) ...))
+  (define&provide kind (λ content (data 'kind content))))
+(repeat define-data
+        area n title prefix year sort-group email
+        sabbatical
+        ;; XXX
+        ugradcom gradcom opscom p&t)
 
-(define-data*
-  area n assoc-chair coord-ugrad coord-msit coord-grad coord-navitas
-  retired emeritus start end title outside prefix year sort-group
-  email chair br)
+(struct status data ())
+(define-syntax-rule (define-status kind)
+  (define&provide kind (status 'kind #f)))
+(repeat define-status
+        assoc assistant full
+        retired emeritus outside deceased
+        start end)
+
+(struct position data ())
+(define-syntax-rule (define-position kind)
+  (define&provide kind (position 'kind #f)))
+(repeat define-position
+        chair assoc-chair
+        coord-ugrad coord-msit coord-grad
+        coord-colloq coord-navitas)
+
+;; chair => p&t
+;; coord-ugrad => ugradcom
+;; coord-grad => gradcom
+;; coord-grad-admissions => gradcom (XXX note when ended)
 
 (struct person-info (content))
 (define-syntax-rule (person . body)
@@ -45,5 +70,72 @@
 (provide prof adjunct staff)
 
 ;; Compiler
+(require racket/match
+         racket/set)
+
+(define (gather-years p)
+  (match p
+    [(person-info p)
+     (gather-years p)]
+    [(data 'year (list (? number? n)))
+     (seteq n)]
+    [(data _ p)
+     (gather-years p)]
+    [(cons x y)
+     (set-union (gather-years x)
+                (gather-years y))]
+    [(or `()
+         #t #f
+         (? number?)
+         (? string?)
+         (? symbol?))
+     (seteq)]))
+
+(define (project y ps)
+  (eprintf "XXX project ~a\n" y)
+  ps)
+
+(define (current-academic-year)
+  (local-require racket/date)
+  (define d (current-date))
+  (define y (date-year d))
+  (define m (date-month d))
+  (if (<= m 9)
+    (sub1 y)
+    y))
+
+;; XXX detect gaps in service
+
+(define (build-committee! y people%y)
+  ;; XXX Commitee page (like
+  ;; https://teaching.cs.uml.edu/~heines/academic/department/Committees2014.jsp)
+  (eprintf "build-committee! ~a XXX\n" y))
+
+(define (build-archive! y people%y)
+  (build-committee! y people%y))
+
+;; XXX two to a page, landscape
+(define (build-board! people%y)
+  (eprintf "build-board! XXX\n"))
+
+(define (build-phone-list! people%y)
+  ;; XXX Get a photo
+  (eprintf "build-phone-list! XXX\n"))
+
+(define (build-face-list! people%y)
+  ;; XXX Get a photo
+  (eprintf "build-face-list! XXX\n"))
+
+(define (build-current! y people%y)
+  (build-board! people%y)
+  (build-phone-list! people%y)
+  (build-face-list! people%y))
+
 (define (build! people)
-  (error 'build! "XXX"))
+  (define all-years (gather-years people))
+  (define cay (current-academic-year))
+  (for ([y (in-set all-years)])
+    (define people%y (project y people))
+    (build-archive! y people%y)
+    (when (= y cay)
+      (build-current! y people%y))))
--- make.rkt
+++ make.rkt
@@ -1,180 +1,913 @@
 #lang at-exp s-exp "dept.rkt"
 
-@prof{
-  @n{Cindy Chen}
-  @area{Database Systems}
-  @area{XML}
-  @email{cchen}
-  @year[2014]
-  @; XXX check start
-  @coord-grad}
+;; XXX UML CS Teaching Award since 2013
 
 @prof{
-  @n{Byung-Guk Kim}
-  @area{High Speed Networking}
-  @email{kim}
-  @year[2014]
-  @assoc-chair}
+  @n{Thomas Costello}
+  @area{Computing Theory}
+  @area{Networking}
+  @e{tom}
+  @full
+  
+  @year[2001]
+  @chair
+  
+  @year[2002]
+
+  @year[2003]
+
+  @year[2004]
+  @chair
+
+  @year[2005]
+  @chair
+  @opscom[2007]
+  @senate[2007]
+
+  @year[2006]
+  @title{UMass Lowell CIO}
+
+  @year[2007]
+
+  @year[2008]
+  
+  @year[2016]
+  @emeritus}
+
+@prof{
+  @n{Georges Grinstein}
+  @area{Visualization}
+  @area{Data Exploration}
+  @area{Bioinformatics}
+  @e{grinstei}
+  @full
+  
+  @year[2001]
+  @p&t[2002]
+  @opscom[2002]
+  
+  @year[2002]
+  @p&t[2004]
+
+  @year[2003]
+
+  @year[2004]
+  @p&t[2006]
+
+  @year[2005]
+  @gradcom[2007]
+
+  @year[2006]
+
+  @year[2007]
+  @sabbatical
+  @gradcom[2009]
+
+  @year[2008]
+  @gradcom['chair]
+  
+  @year[2016]
+  @emeritus}
+
+@prof{
+  @n{Giampiero Pecelli}
+  @area{Network Modeling}
+  @area{Symbolic Computation}
+  @e{giam}
+  @full
+  
+  @year[2001]
+  @p&t[2003]
+  @gradcom['chair]
+  @coord-grad
+  
+  @year[2002]
+  @ugradcom[2004]
+
+  @year[2003]
+
+  @year[2004]
+  @ugradcom[2006]
+
+  @year[2005]
+  @coord-grad-quals[2007]
+
+  @year[2006]
+  @ugradcom[2008]
+  @coord-grad-quals[2007]
+
+  @year[2007]
+  @coord-grad-ms
+  @gradcom['chair]
+  @college-personnel[2008]
+
+  @year[2008]
+  @coord-grad
+  @coord-grad-ms
+  @college-grad
+  
+  @year[2016]
+  @emeritus}
 
 @prof{
   @n{Jie Wang}
   @area{Computational Complexity}
   @area{Network Security}
-  @email{wang}}
+  @e{wang}
+  @full
+  
+  @year[2001]
+  @award-teaching
+  @gradcom[2003]
+  @coord-colloq[2003]
+  
+  @year[2002]
 
-@prof{@n{David Adams}
-  @area{Scientific Computing}
-  @area{CS Education}
-  @email{dbadams}
-  @year[2014]
-  @coord-ugrad}
+  @year[2003]
+  @p&t[2005]
+  @gradcom[2005]
+  @coord-grad-quals[2005]
+
+  @year[2004]
+  @coord-grad-quals[2005]
+
+  @year[2005]
+  @gradcom[2007]
+
+  @year[2006]
+  @chair
+
+  @year[2007]
+
+  @year[2008]
+  @senate[2009]}
 
 @prof{
-  @n{William Moloney}
-  @area{Operating Systems}
-  @email{bill}
+  @n{James Canning}
+  @area{Compilers}
+  @area{Parallel Systems}
+  @e{canning}
+  @assoc
+
+  @year[1999]
+  @award-teaching
+
+  @year[2000]
+  @award-teaching
+  
+  @year[2001]
+  @ugradcom[2003]
+  
+  @year[2002]
+  @gradcom['chair]
+
+  @year[2003]
+  @coord-grad
+
+  @year[2004]
+  @p&t[2006]
+  @gradcom['chair]
+  @coord-grad
+
+  @year[2005]
+
+  @year[2006]
+  @coord-grad
+  @gradcom['chair]
+
+  @year[2007]
+
+  @year[2008]
+  @opscom[2010]
+  
+  @year[2016]
+  @title{Dean, University Honors Program}
+  @outside}
+
+@prof{
+  @n{Jesse Heines}
+  @area{Interdisciplinary Computing}
+  @area{Music}
+  @area{Graphical User Interfaces}
+  @e{heines}
+  @assoc
+
+  @year[2000]
+  @award-teaching
+  
+  @year[2001]
+  @gradcom[2002]
+  @ugradcom['chair]
+  @ugradcom[2003]
+  
+  @year[2002]
+  @gradcom[2004]
+
+  @year[2003]
+  @award-teaching
+  @p&t[2005]
+
+  @year[2004]
+  @ugradcom['chair]
+  @ugradcom[2006]
+  
+  @year[2005]
+  @award-teaching
+
+  @year[2006]
+  @coord-ugrad
+  @ugradcom['chair]
+
+  @year[2007]
+
+  @year[2008]
+  @ugradcom['chair]
+  @student-advisory-board
+  @college-ugrad
+
+  @year[2009]
+  @full
+  
+  @year[2016]
+  @retired}
+
+@prof{
+  @n{Byung-Guk Kim}
+  @area{High Speed Networking}
+  @e{kim}
+  @assoc
+  
+  @year[2001]
+  @sabbatical
+  
+  @year[2002]
+  @ugradcom[2004]
+
+  @year[2003]
+
+  @year[2004]
+
+  @year[2005]
+
+  @year[2006]
+
+  @year[2007]
+  @ugradcom[2009]
+
+  @year[2008]
+  
   @year[2014]
-  @coord-msit}
+  @assoc-chair}
 
 @prof{
   @n{Haim Levkowitz}
   @area{Visualization}
   @area{Graphics}
   @area{Web Technology}
-  @email{haim}
+  @e{haim}
+  @assoc
+  
+  @year[2001]
+  @p&t[2003]
+  @gradcom[2002]
+  
+  @year[2002]
+  @opscom[2004]
+
+  @year[2003]
+  @gradcom[2005]
+
+  @year[2004]
+  @sabbatical
+
+  @year[2005]
+  @ugradcom[2007]
+  @senate[2007]
+
+  @year[2006]
+
+  @year[2007]
+  @gradcom[2009]
+
+  @year[2008]
+  @senate[2009]
+  @acm-adivsor
+  
   @year[2016]
   @chair}
+
+@prof{
+  @n{William Moloney}
+  @area{Operating Systems}
+  @e{bill}
+  @assoc
+
+  @year[1998]
+  @award-teaching
+
+  @year[2001]
+  @gradcom[2003]
+  @opscom[2002]
+
+  @year[2002]
+  @award-teaching
+  @p&t[2004]
+
+  @year[2003]
+  @opscom[2005]
+
+  @year[2004]
+
+  @year[2005]
+  @ugradcom[2007]
+  @opscom[2007]
+
+  @year[2006]
+
+  @year[2007]
+  @ugradcom[2009]
+  @opscom['chair]
+
+  @year[2008]
+  @award-teaching
+  @opscom['chair]
+
+  @year[2012]
+  @award-teaching
+  
+  @year[2014]
+  @coord-msit}
+
+@prof{
+  @n{Cindy Chen}
+  @area{Database Systems}
+  @area{XML}
+  @e{cchen}
+  
+  @year[2002]
+  @assistant
+
+  @year[2003]
+  @opscom[2005]
+
+  @year[2004]
+
+  @year[2005]
+  @gradcom[2007]
+
+  @year[2006]
+  @coord-colloq[2007]
+
+  @year[2007]
+  @gradcom[2008]
+  @coord-colloq[2008]
+
+  @year[2008]
+  @opscom[2010]
+  @senate[2009]
+
+  @year[2009]
+  @assoc
+
+  @year[2014]
+  @coord-grad}
+
+@prof{
+  @n{Guanling Chen}
+  @area{Wireless Networks}
+  @e{glchen}
+
+  @year[2005]
+  @assistant
+
+  @year[2006]
+  @opscom[2008]
+
+  @year[2007]
+
+  @year[2008]
+  @gradcom[2010]
+  @newsletter[2009]}
+
+@prof{
+  @n{Kajal Claypool}
+  @e{kajal}
+  @assistant
+  
+  @year[2001]
+  @ugradcom[2003]
+
+  @year[2002]
+  @award-teaching
+
+  @year[2003]
+  @ugradcom[2005]
+
+  @year[2004]
+  @award-teaching
+
+  @year[2005]
+  @ugradcom[2007]
+
+  @year[2006]
+  @end}
+
+@prof{
+  @n{Adam Elbirt}
+  @e{aelbirt}
+
+  @year[2005]
+  @assistant
+
+  @year[2006]
+
+  @year[2007]
+  @gradcom[2009]
+
+  @year[2008]
+  @end}
+
+@prof{
+  @n{Karen Daniels}
+  @area{Applied Computational Geometry}
+  @e{kdaniels}
+  @assistant
+  
+  @year[2001]
+  @opscom[2002]
+
+  @year[2002]
+  @gradcom[2004]
+
+  @year[2003]
+
+  @year[2004]
+  @gradcom[2006]
+
+  @year[2005]
+  @opscom[2007]
+
+  @year[2006]
+  @assoc
+  @gradcom[2008]
+
+  @year[2007]
+  @coord-grad-phd
+
+  @year[2008]
+  @gradcom[2010]
+  @coord-grad-phd
+  @wics
+  
+  @year[2009]
+  @award-teaching
+  
+  @year[2016]
+  @retired}
+
+@prof{
+  @n{Allyn Dimock}
+  @e{dimock}
+
+  @year[2002]
+  @assistant
+
+  @year[2003]
+  @opscom[2005]
+
+  @year[2004]
+  @gradcom[2006]
+
+  @year[2005]
+  @end}
+
+@prof{
+  @n{Ben Liu}
+  @area{Computer Networking}
+  @e{bliu}
+
+  @year[2003]
+  @assistant
+
+  @year[2004]
+  @opscom[2006]
+
+  @year[2005]
+
+  @year[2006]
+  @gradcom[2008]
+
+  @year[2007]
+
+  @year[2008]
+  @gradcom[2010]
+
+  @year[2011]
+  @award-teaching}
+
+@prof{
+  @n{Gary Livingston}
+  @e{gary}
+
+  @year[2002]
+  @assistant
+
+  @year[2003]
+  @ugradcom[2005]
+  @coord-colloq[2005]
+
+  @year[2004]
+
+  @year[2005]
+
+  @year[2006]
+
+  @year[2007]
+  @senate[2008]
+
+  @year[2008]
+  @senate[2009]
+
+  @year[2009]
+  @end}
+
+@prof{
+  @n{David Martin}
+  @e{dm}
+
+  @year[2002]
+  @assistant
+  @opscom[2004]
+
+  @year[2003]
+  @award-teaching
+
+  @year[2004]
+  @ugradcom[2006]
+
+  @year[2005]
+  @leave
+
+  @year[2006]
+
+  @year[2007]
+  @end}
+
+@prof{
+  @n{Fred Martin}
+  @area{Embedded Systems}
+  @area{Design Environments}
+  @e{fredm}
+
+  @year[2002]
+  @assistant
+  @ugradcom[2004]
+
+  @year[2003]
+
+  @year[2004]
+  @gradcom[2006]
+  @award-teaching
+
+  @year[2005]
+  @award-teaching
+
+  @year[2006]
+  @ugradcom[2008]
+
+  @year[2007]
+
+  @year[2008]
+  @assoc
+  @ugradcom[2010]
+  @coord-colloq[2009]
+  
+  @year[2016]
+  @title{Director of Student Success, College of Sciences}}
+
+@prof{
+  @n{Li Xu}
+  @e{xu}
+
+  @year[2003]
+  @assistant
+
+  @year[2004]
+
+  @year[2005]
+  @opscom[2007]
+  @senate[2007]
+
+  @year[2006]
+
+  @year[2007]
+  @award-teaching
+  @ugradcom[2009]
+
+  @year[2008]
+
+  @year[2009]
+  @end}
+
+@prof{
+  @n{Holly Yanco}
+  @area{Robotics}
+  @area{Assitive Technology}  
+  @e{holly}
+  @assistant
+  
+  @year[2001]
+  @award-teaching
+  @ugradcom[2002]
+
+  @year[2002]
+  @gradcom[2004]
+
+  @year[2003]
+
+  @year[2004]
+  @opscom[2006]
+  @coord-colloq[2006]
+
+  @year[2005]
+  @senate[2007]
+
+  @year[2006]
+  @assoc
+  @opscom[2008]
+
+  @year[2007]
+  @senate[2008]
+
+  @year[2008]
+  @sabbatical
+  
+  @year[2016]
+  @title{Director, NERVE Center}}
+
+@prof{
+  @n{Bob Lechner}
+  @e{lechner}
+  
+  @year[2002]
+  @emeritus
+
+  @year[2006]}
+
+@prof{
+  @n{Marian Williams}
+  @e{williams}
+  @assoc
+
+  @year[1996]
+  @award-teaching
+  
+  @year[2001]
+  @sabbatical
+
+  @year[2002]
+  @coord-grad-admissions
+
+  @year[2003]
+  @emeritus
+
+  @year[2004]
+  @coord-grad-admissions
+
+  @year[2006]}
+
+@prof{
+  @n{Charlie Steele}
+  @assoc
+
+  @year[1997]
+  @award-teaching
+  
+  @year[2001]
+  @deceased}
+
+@adjucnt{
+  @n{Adrien Grisé}
+  @e{agrise}
+
+  @year[2003]
+
+  @year[2004]
+
+  @year[2005]
+  @end}
+
+@adjunct{
+  @n{Hugh Masterman}
+  @email{hcm@"@"mitre.org}
+
+  @year[2003]
+
+  @year[2004]
+
+  @year[2005]
+  @end}
+
+@adjunct{
+  @n{Dave Pitts}
+  @e{pitts}
+
+  @year[2003]
+
+  @year[2004]
+
+  @year[2005]
+  @end}
+
+@adjunct{
+  @n{Paul Wexelblat}
+  @e{wex}
+
+  @year[2003]
+
+  @year[2004]
+
+  @year[2005]
+  @end}
+
+@adjunct{
+  @n{Kary Robertson}
+  @e{kary}
+
+  @year[2005]
+
+  @year[2006]
+
+  @year[2007]
+  @end}
+
+@adjunct{
+  @n{Peter Weston}
+  @e{weston}
+
+  @year[2005]
+  
+  @year[2006]
+
+  @year[2007]
+  @end}
+
+@adjunct{
+  @n{Martin Schedlbauer}
+  @e{mschedl}
+
+  @year[2006]
+
+  @year[2007]
+  @end}
+
+@prof{
+  @n{Ray Gumb}
+  @e{gumb}
+  @full
+  
+  @year[2001]
+  @opscom[2002]
+
+  @year[2002]
+  @retired}
+
+@prof{
+  @n{Pat Krolak}
+  @e{pkrolak}
+  @full
+  
+  @year[2001]
+  @ugradcom[2002]
+
+  @year[2002]
+  @retired
+
+  @year[2006]
+  @emeritus}
+
+@prof{
+  @n{John Sieg}
+  @e{john}
+  @assoc
+  
+  @year[2001]
+  @p&t[2002]
+  @coord-ugrad
+
+  @year[2002]
+
+  @year[2003]
+
+  @year[2004]
+  @coord-ugrad
+
+  @year[2005]
+  @deceased}
+
+@prof{
+  @n{Stu Smith}
+  @e{stu}
+
+  @year[2006]
+  @emeritus}
 
 @prof{
   @n{Xinwen Fu}
   @area{Network Security}
   @area{Privacy}
-  @email{xinwenfu}}
+  @e{xinwenfu}
+
+  @year[2008]
+  @assistant
+  @ugradcom[2010]
+
+  @year[2010]
+  @award-teaching}
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+@prof{@n{David Adams}
+  @area{Scientific Computing}
+  @area{CS Education}
+  @e{dbadams}
+
+  @year[2013]
+  @award-teaching
+  
+  @year[2014]
+  @coord-ugrad}
 
 @prof{
   @n{Tingjian Ge}
   @area{Data Management Systems}
-  @email{ge}}
+  @e{ge}
+
+  @year[2013]
+  @award-teaching}
 
 @prof{
-  @n{Karen Daniels}
-  @area{Applied Computational Geometry}
-  @email{kdaniels}
-  @year[2016]
-  @retired}
-
-@prof{
-  @year[2015]
   @n{Ayat Hatem}
   @area{Biological Networks Analysis}
   @area{Big Data}
   @area{Sequence Mapping}
-  @email{ahatem}
+  @e{ahatem}
+
+  @year[2015]
+  @ntt
+
   @year[2016]
   @end}
-
-@prof{
-  @n{Guanling Chen}
-  @area{Wireless Networks}
-  @email{glchen}}
 
 @prof{
   @n{Yu Cao}
   @area{Big Data}
   @area{Computational Intelligence}
   @area{Multimedia Computing}
-  @email{ycao}}
-
-@prof{
-  @n{Ben Liu}
-  @area{Computer Networking}
-  @email{bliu}}
-
-@prof{
-  @n{Fred Martin}
-  @area{Embedded Systems}
-  @area{Design Environments}
-  @email{fredm}
-  @title{Director of Student Success, College of Sciences}}
+  @e{ycao}}
 
 @prof{
   @n{Jay McCarthy}
   @area{Programming Languages}
   @area{Formal Verification}
   @area{CS Education}
-  @email{jmccarth}
+  @e{jmccarth}
   @year[2016]
   @coord-navitas}
 
 @prof{
   @n{Anna Rumshisky}
   @area{Natural Language Processing}
-  @email{arum}}
+  @e{arum}}
 
 @prof{
   @n{Kate Saenko}
   @area{Computer Vision}
   @area{Machine Learning}
-  @email{saenko}
+  @e{saenko}
   @year[2016]
   @end}
 
-@prof{@n{Holly Yanko}
-  @title{Director, NERVE Center}
-  @area{Robotics}
-  @area{Assitive Technology}
-  @email{holly}}
-
-@prof{@n{Jesse Heines}
-  @area{Interdisciplinary Computing}
-  @area{Music}
-  @area{Graphical User Interfaces}
-  @email{heines}
-  @year[2016]
-  @retired}
-
-@prof{@n{Giampiero Pecelli}
-  @area{Network Modeling}
-  @area{Symbolic Computation}
-  @email{giam}
-  @emeritus}
-
-@prof{@n{Georges Grinstein}
-  @area{Visualization}
-  @area{Data Exploration}
-  @area{Bioinformatics}
-  @email{grinstein}
-  @emeritus}
-
-@prof{@n{Thomas Costello}
-  @area{Computing Theory}
-  @area{Networking}
-  @email{tom}
-  @emeritus}
-
-@prof{@n{James Canning}
-  @area{Compilers}
-  @area{Parallel Systems}
-  @email{canning}
-  @title{Dean, University Honors Program}
-  @outside}
-
-@adjunct{@n{David Hendrickson}
+@adjunct{
+  @n{David Hendrickson}
   @area{Computer Architecture}
   @area{Assembly Language}
-  @email{dhendric}}
+  @e{dhendric}}
 
-@staff{@n{Phyllis Procter}
+@staff{
+  @n{Phyllis Procter}
   @prefix{Ms.}
-  @title{Community Partnerships @br Program Manager}
-  @email{pprocter}}
+  @title{Community Partnerships @"\n" Program Manager}
+  @e{pprocter}}
 
-@staff{@n{Karen Volis}
+@staff{
+  @n{Karen Volis}
   @prefix{Ms.}
   @title{Administrative Assisant}
-  @email{dept}}
+  @e{dept}}
 
-@staff{@n{Tuyen Nguyen}
+@staff{
+  @n{Tuyen Nguyen}
   @prefix{Mr.}
   @title{Systems Manager}
-  @email{tuyen}}
+  @e{tuyen}}
